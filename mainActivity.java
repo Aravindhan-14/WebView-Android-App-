74c639651aa9248762ebb41d108bfc61afe2fe39
M Aravindhan 07 <aravindh2k05@gmail.com>
2025-07-17 20:20:19 +0530

    mainActivity.java

---
+++ mainActivity.java
@@ -0,0 +1,44 @@
+package com.example.google;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    private String webUrl="https://trainingtrains.com/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.myWebView);
+        webView.loadUrl(webUrl);
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+    }
+        else
+            super . onBackPressed();
+}
+}
